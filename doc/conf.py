07fbd18f1af816488e1f79f62581550721c1446a
Vladimir DOMBROVSKI <vladimir@openio.io>
2017-12-06 15:16:43 +0100

    Add redirection to landing page

--- doc/conf.py
+++ doc/conf.py
@@ -1,7 +1,7 @@
 html_context = {
     'css_files': ['_static/styles.css'],
     'oio_versions': ['16.10', '17.04', '17.10'],
-    'default_path': ['source/admin-guide/configuration_ec.html']
+    'landing_page': 'source/sandbox-guide/quickstart.html'
 }
 extensions = [
         'breathe',
